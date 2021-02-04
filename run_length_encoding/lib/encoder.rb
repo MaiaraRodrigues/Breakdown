1b2f1a0c7346095688d344a704e9144beb96335a
Maiara <maiarinhah.eu@gmail.com>
2021-02-04 17:26:26 +0000

    Third exercise passing the tests

--- run_length_encoding/lib/encoder.rb
+++ run_length_encoding/lib/encoder.rb
@@ -6,4 +6,10 @@ class Encoder
   def get_first_letter(first)
     first.chr
   end 
+
+  def get_first_letter_and_length(string)
+    result = []
+    result << string.chr << string.length
+    result.join
+  end 
 end
